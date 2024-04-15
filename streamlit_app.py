da560a54331121caeb35dc602d42b1b142e61412
yinchenchang0918 <167038822+yinchenchang0918@users.noreply.github.com>
2024-04-15 13:58:31 +0800

    Update streamlit_app.py-move fruityvice to fruit_chosen block

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Custom Your Snoothie :cup_with_straw:")
@@ -27,6 +28,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
@@ -45,8 +48,6 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 
-# New section to display fruityvice nutrition information
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response.json())
-fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width=True)
+
+
+
